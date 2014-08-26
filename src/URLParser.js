87ff909cfe19b29b1e8f2fe14c3992b41d52f52f
amigo237 <540908525@qq.com>
2014-08-26 20:28:24 +0800

    修改代码格式，增加getHashData的可设置项

--- src/URLParser.js
+++ src/URLParser.js
@@ -5,20 +5,20 @@
  * http://blog.stevenlevithan.com/archives/parseuri
  */
 
-(function( window ) {
+(function (window) {
 
-    function httpBuildQuery( data ) {
+    function httpBuildQuery (data) {
         var arr = [];
-        for(var key in data) {
-            arr.push( key + "=" + data[key] );
+        for (var key in data) {
+            arr.push(key + "=" + data[key]);
         }
         return arr.join("&");
     }
 
     //example url: http://amigo237:abc123@www.example.com/path/index.html?name=amigo237&sex=m#test=hash&age=25
-    var URLParser = function( url ) {
-        if( !( this instanceof URLParser ) ) {
-            var obj = new URLParser( url );
+    var URLParser = function (url) {
+        if (!(this instanceof URLParser)) {
+            var obj = new URLParser(url);
             return obj;
         }
         url = url || location.href;
@@ -36,54 +36,54 @@
         }
     };
 
-    URLParser.prototype.parse = function( url ) {
+    URLParser.prototype.parse = function (url) {
         url = url || this.url;
         var	option = URLParser.options,
             model = option.parser[option.strictMode ? "strict" : "loose"].exec(url),
             i = 14;
 
-        while( i-- ) {
+        while (i--) {
             this[option.key[i]] = model[i] || "";
         }
     };
 
-    URLParser.prototype.getQueryData = function( key ) {
+    URLParser.prototype.getQueryData = function (key, raw) {
         var queryData = {};
-        this.query.replace( URLParser.options.query, function( $0, $1, $2 ) {
-            if($1) {
-                queryData[$1] = $2;
+        this.query.replace(URLParser.options.query, function ($0, $1, $2) {
+            if ($1) {
+                queryData[$1] = raw ? $2 : decodeURIComponent($2);
             } 
         });
         return key ? queryData[key] : queryData;
     };
 
-    URLParser.prototype.getHashData = function( key ) {
+    URLParser.prototype.getHashData = function (key) {
         var hashData = {};
-        this.anchor.replace(URLParser.options.query, function( $0, $1, $2 ) {
-            if($1) {
+        this.anchor.replace(URLParser.options.query, function ($0, $1, $2) {
+            if ($1) {
                 hashData[$1] = $2;
             } 
         });
         return key ? hashData[key] : hashData;
     };
     
-    URLParser.prototype.setQuery = function( data ) {
+    URLParser.prototype.setQuery = function (data) {
         var queryData = this.getQueryData();
         var isQueryDataChange = false,
             queryString;
 
-        for(var key in data) {
-            if( data[key] !== queryData[key] ) {
+        for (var key in data) {
+            if (data[key] !== queryData[key]) {
                 queryData[key] = data[key];
                 isQueryDataChange = true;
             }
         }
 
-        if(isQueryDataChange) {
+        if (isQueryDataChange) {
             queryString = httpBuildQuery(queryData);
-            if( this.query !== "" ) {
+            if (this.query !== "") {
                 var queryReg = /\?([^#]*)/;
-                this.url = this.url.replace( queryReg, "?" + queryString );
+                this.url = this.url.replace(queryReg, "?" + queryString);
             }
             else {
                 this.url = this.url + "?" + queryString;
@@ -93,23 +93,23 @@
         return this;
     }
 
-    URLParser.prototype.setHash = function( data ) {
+    URLParser.prototype.setHash = function (data) {
         var hashData = this.getHashData();
         var isHashDataChange = false,
             hashString;
 
-        for(var key in data) {
-            if( data[key] !== hashData[key] ) {
+        for (var key in data) {
+            if (data[key] !== hashData[key]) {
                 hashData[key] = data[key];
                 isHashDataChange = true;
             }
         }
 
-        if(isHashDataChange) {
+        if (isHashDataChange) {
             hashString = httpBuildQuery(hashData);
-            if( this.anchor !== "" ) {
+            if (this.anchor !== "") {
                 var hashReg = /#(.*)/;
-                this.url = this.url.replace( hashReg, "#" + hashString );
+                this.url = this.url.replace(hashReg, "#" + hashString);
             }
             else {
                 this.url = this.url + "#" + hashString;
@@ -120,4 +120,4 @@
     }
 
     window["URLParser"] = window.URLParser || URLParser;
-})( window );
+})(window);
